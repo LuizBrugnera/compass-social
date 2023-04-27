3dcace0832c0a657271e6fcbcdf5fe81e21b6c35
Luiz Brugnera <luiz.r.brugnera@gmail.com>
2023-04-26 21:28:03 -0300

    feat  - creating the home page template

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import './App.css';
 import React from 'react';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// components
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+import Home from './pages/Home';
+// css
+import './App.css';
 
 function App(){
   return (
     <div className="App">
-      <Login/>
+      
     </div>
   );
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import HomeLayout from '../components/templates/HomeLayout'
+
+const Home = () => {
+  return (
+    <HomeLayout/>
+  )
+}
+
+export default Home
