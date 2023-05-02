e9a96ef0240c35a6c8859e57e08edc79c22b1b94
Luiz Brugnera <luiz.r.brugnera@gmail.com>
2023-05-02 11:12:03 -0300

    feat - LoginSignup Layout

---
+++ front-end/src/components/templates/LoginSignupLayout.tsx
@@ -0,0 +1,39 @@
+import React, { ReactNode } from 'react';
+// css
+import './LoginSignupLayout.css';
+// assets
+import compass_img from "../../assets/compass.png";
+// types
+type LoginSignupLayoutProps = {
+    children: ReactNode;
+    text: string;
+}
+
+const LoginSignupLayout = ({ children, text } : LoginSignupLayoutProps) => {
+  return (
+    <main className="main">
+
+        <section className="section-content">
+            <div className='main-title'>
+    
+                <h1 className="h1-title">Olá,</h1>
+    
+                <p className="p-title">{text}</p>
+    
+            </div>
+    
+            <div>
+    
+                {children}
+            </div>
+        </section>
+    
+        <section className="section-images">
+            <img className="notbook-img" src={compass_img} alt="Notbook"/>
+        </section>
+
+    </main>
+  )
+}
+
+export default LoginSignupLayout
