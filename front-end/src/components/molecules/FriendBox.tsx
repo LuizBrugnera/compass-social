a65e6159e6f8bf866742efc6cedf66d4c8912be8
Luiz Brugnera <luiz.r.brugnera@gmail.com>
2023-05-29 15:40:04 -0300

    feat - integrating friendbox with the back end

--- front-end/src/components/molecules/FriendBox.tsx
+++ front-end/src/components/molecules/FriendBox.tsx
@@ -4,74 +4,85 @@ import styles from "./FriendBox.module.css";
 // assets
 import arrowdown_icon from "../../assets/arrowdown_icon.png";
 import arrowup_icon from "../../assets/arrowup_icon.png";
+import { useAuth } from "../../security/AuthProvider";
 
 const FriendBox = ({ userList, type }: any) => {
-  const allFriends = () => {
-    return userList.map((user: any, index: number) => {
-      return (
-        <li className={styles.container_item} key={index}>
-          <div className={styles.friend_img}>
-            <img
-              className={styles.friend_img}
-              src={user.profile_photo}
-              alt="friend"
-            />
-          </div>
-          <span className={styles.font_14}>{user.name}</span>
-        </li>
-      );
-    });
-  };
+  const { userAuth } = useAuth();
+  const myUser = userAuth?.user;
 
-  const firstFourFriends = () => {
-    return userList.map((user: any, index: number) => {
-      if (index < 4) {
+    const allFriends = () => {
+      return userList.map((user: any, index: number) => {
+        if (user.user === myUser!.user) return <React.Fragment key={index} />;
+
         return (
           <li className={styles.container_item} key={index}>
-          <div className={styles.friend_img}>
-            <img
-              className={styles.friend_img}
-              src={user.profile_photo}
-              alt="friend"
-            />
-          </div>
-          <span className={styles.font_14}>{user.name}</span>
-        </li>
+            <div className={styles.friend_img}>
+              <img
+                className={styles.friend_img}
+                src={user.profile_photo}
+                alt="friend"
+              />
+            </div>
+            <span className={styles.font_14}>{user.name}</span>
+          </li>
         );
+      });
+    };
+
+    const firstFourFriends = () => {
+      return userList.map((user: any, index: number) => {
+        if (user.user === myUser!.user) return <React.Fragment key={index} />;
+
+        if (index < 4) {
+          return (
+            <li className={styles.container_item} key={index}>
+              <div className={styles.friend_img}>
+                <img
+                  className={styles.friend_img}
+                  src={user.profile_photo}
+                  alt="friend"
+                />
+              </div>
+              <span className={styles.font_14}>{user.name}</span>
+            </li>
+          );
+        }
+        return null; 
+      });
+    };
+
+    const [changeButton, setChangeButton] = useState("firstFour");
+    const [arrowIcon, setArrowIcon] = useState(arrowup_icon);
+    const [showBox, setShowBox] = useState(false);
+
+    const handlerChange = () => {
+      if (changeButton === "all") {
+        setChangeButton("firstFour");
+      } else {
+        setChangeButton("all");
       }
-    });
+      setShowBox(!showBox);
+    };
+
+    return (
+      <div className={`${styles.friend_box} ${styles[showBox ? "show" : ""]}`}>
+        <li
+          className={styles.drop_box}
+          onClick={() => {
+            handlerChange();
+            setArrowIcon(
+              arrowdown_icon === arrowIcon ? arrowup_icon : arrowdown_icon
+            );
+          }}
+        >
+          <h2 className={styles.font_16}>Meus Amigos</h2>
+          <img src={arrowIcon} alt="arrow down icon" />
+        </li>
+        {type === "friend-list" && userList && (
+          <>{changeButton === "all" ? allFriends() : firstFourFriends()}</>
+        )}
+      </div>
+    );
   };
 
-  const [changeButton, setChangeButton] = useState("firstFour");
-  const [arrowIcon, setArrowIcon] = useState(arrowup_icon);
-  const [showBox, setShowBox] = useState(false);
-
-  const handlerChange = () => {
-    if (changeButton === "all") {
-      setChangeButton("firstFour");
-    } else {
-      setChangeButton("all");
-    }
-    setShowBox(!showBox);
-  };
-  
-  return (
-    <div className={`${styles.friend_box} ${styles[showBox ? "show" : ""]}`}>
-      <li
-        className={styles.drop_box}
-        onClick={() => {
-          handlerChange();
-          setArrowIcon(arrowdown_icon === arrowIcon ? arrowup_icon : arrowdown_icon);
-        }}
-      >
-        <h2 className={styles.font_16}>Meus Amigos</h2>
-        <img src={arrowIcon} alt="arrow down icon" />
-      </li>
-      {type === "friend-list" && userList && (
-        <>{changeButton === "all" ? allFriends() : firstFourFriends()}</>
-      )}
-    </div>
-  );
-};
-
-export default FriendBox;
+  export default FriendBox;
