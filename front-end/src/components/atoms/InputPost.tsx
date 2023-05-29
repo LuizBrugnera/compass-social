8d10956b9c87fc9916003ddc004ae0f87335dc3d
Luiz Brugnera <luiz.r.brugnera@gmail.com>
2023-05-29 15:39:15 -0300

    feat - integrating post creation with the back end~

--- front-end/src/components/atoms/InputPost.tsx
+++ front-end/src/components/atoms/InputPost.tsx
@@ -10,7 +10,8 @@ import clip_icon from "../../assets/clip_icon.png";
 import point_icon from "../../assets/point_icon.png";
 import smile_icon from "../../assets/smile_icon.png";
 /// auth user
-import { useAuth } from '../../AuthProvider';
+import { useAuth } from "../../security/AuthProvider";
+import { PostService } from "../services/PostService";
 
 // types
 type InputPostType = {
@@ -18,18 +19,19 @@ type InputPostType = {
   setPostList?: any;
 };
 const InputPost = ({ message, setPostList }: InputPostType) => {
-  let { user } = useAuth();
+  let { userAuth } = useAuth();
 
-  if(user === null) {
-    user = {
-      name: "Usuário",
-      user: "usuario",
-      birthdate: "1912-06-23",
-      email: "usuario@gmail.com",
-      password: "usuario",
-      profile_photo: user_img,
-    }
-  }
+  let user = userAuth?.user
+    ? userAuth.user
+    : {
+        _id: "Usuário",
+        name: "Usuário",
+        user: "usuario",
+        birthdate: "1912-06-23",
+        email: "usuario@gmail.com",
+        password: "usuario",
+        profile_photo: user_img,
+      };
 
   const handlerImgInput = () => {
     setImgInput(!imgInput);
@@ -38,33 +40,43 @@ const InputPost = ({ message, setPostList }: InputPostType) => {
     setLocationInput(!locationInput);
   };
 
-  const handlerAddPost = () => {
+  const handlerAddPost = async () => {
     if (description !== "" || url_imagem !== "") {
-      const now = new Date();
+      const postRes = (await PostService.createPost(userAuth?.token!, {
+        user: userAuth?.user?._id,
+        description: description,
+        url_imagem: url_imagem,
+        post_date: new Date(),
+      })) as any;
+
       setPostList((prevState: any) => {
-        return [
-          {
-            user: user?.user,
-            description: description,
-            url_imagem: url_imagem,
-            post_date: now.getTime(),
-            likes: 0,
-          },
-          ...prevState,
-        ];
+        return [postRes.response, ...prevState];
       });
+
+      setDescription("");
+      setUrl_imagem("");
     }
   };
+
+  const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      handlerAddPost();
+    }
+  };
+
   const [imgInput, setImgInput] = useState(false);
   const [locationInput, setLocationInput] = useState(false);
-  const [description, setDescription] = useState("");
-  const [url_imagem, setUrl_imagem] = useState("");
-  /// const [location, setLocation] = useState("");
+  const [description, setDescription] = useState("" as string);
+  const [url_imagem, setUrl_imagem] = useState("" as string);
 
   return (
     <>
       <div className={styles.container_add}>
-        <img className={styles.friend_img} src={user.profile_photo} alt="friend" />
+        <img
+          className={styles.friend_img}
+          src={user.profile_photo}
+          alt="friend"
+        />
         <input
           className={styles.input_post}
           type="text"
@@ -72,61 +84,73 @@ const InputPost = ({ message, setPostList }: InputPostType) => {
           onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
             setDescription(e.target.value)
           }
+          value={description}
+          onKeyDown={handleKeyPress}
         />
       </div>
 
-          {imgInput && (
-            <div className={`${styles.container_add} ${styles.pad16}`}>
-              <img className={styles.friend_img} src={user.profile_photo} alt="friend" />
-              <input
-                className={styles.input_post}
-                type="text"
-                placeholder={"Adicione o url da imagem"}
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                  setUrl_imagem(e.target.value)
-                }
-              />
-            </div>
-          )}
+      {imgInput && (
+        <div className={`${styles.container_add} ${styles.pad16} `}>
+          <img
+            className={`${styles.friend_img} ${styles.hidden}`}
+            src={user.profile_photo}
+            alt="friend"
+          />
+          <input
+            className={styles.input_post}
+            type="text"
+            placeholder={"Adicione o url da imagem"}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setUrl_imagem(e.target.value)
+            }
+            value={url_imagem}
+            onKeyDown={handleKeyPress}
+          />
+        </div>
+      )}
 
-          {locationInput && (
-            <div className={`${styles.container_add} ${imgInput ? styles.pad16_bottom : styles.pad16}`}>
-              <img className={styles.friend_img} src={user.profile_photo} alt="friend" />
-              <input
-                className={styles.input_post}
-                type="text"
-                placeholder={"Adicione a localização"}
-              />
-            </div>
-          )}
+      {locationInput && (
+        <div
+          className={`${styles.container_add} ${
+            imgInput ? styles.pad16_bottom : styles.pad16
+          }`}
+        >
+          <img
+            className={`${styles.friend_img} ${styles.hidden}`}
+            src={user.profile_photo}
+            alt="friend"
+          />
+          <input
+            className={styles.input_post}
+            type="text"
+            placeholder={"Adicione a localização"}
+          />
+        </div>
+      )}
 
-          <div className={styles.container_flex_between}>
-            <div className={styles.container_icons}>
-              <img className={styles.icon_24} src={cam_icon} alt="cam icon" />
-              <img
-                className={styles.icon_24}
-                src={img_icon}
-                alt="img icon"
-                onClick={handlerImgInput}
-              />
-              <img className={styles.icon_24} src={clip_icon} alt="clip icon" />
-              <img
-                className={styles.icon_24}
-                src={point_icon}
-                alt="point icon"
-                onClick={handlerLocationInput}
-              />
-              <img
-                className={styles.icon_24}
-                src={smile_icon}
-                alt="smile icon"
-              />
-            </div>
-            <button className={styles.btn_post} onClick={handlerAddPost}>
-              Postar
-            </button>
-          </div>
-        </>
+      <div className={styles.container_flex_between}>
+        <div className={styles.container_icons}>
+          <img className={styles.icon_24} src={cam_icon} alt="cam icon" />
+          <img
+            className={styles.icon_24}
+            src={img_icon}
+            alt="img icon"
+            onClick={handlerImgInput}
+          />
+          <img className={styles.icon_24} src={clip_icon} alt="clip icon" />
+          <img
+            className={styles.icon_24}
+            src={point_icon}
+            alt="point icon"
+            onClick={handlerLocationInput}
+          />
+          <img className={styles.icon_24} src={smile_icon} alt="smile icon" />
+        </div>
+        <button className={styles.btn_post} onClick={handlerAddPost}>
+          Postar
+        </button>
+      </div>
+    </>
   );
 };
 
